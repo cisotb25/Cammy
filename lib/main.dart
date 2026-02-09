0754ab6de3a6658d778af0364f2f6f8884bba241
Narciso Barroso <up202305431@up.pt>
2026-02-09 03:53:57 +0000

    Cleaned up main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_card_swiper/flutter_card_swiper.dart';
-import 'photo_card.dart';
+import 'screens/main_layout.dart';
 
-void main() => runApp(const MaterialApp(home: CammyHome()));
+void main() {
+  runApp(const CammyApp());
+}
 
-class CammyHome extends StatelessWidget {
-  const CammyHome({super.key});
+class CammyApp extends StatelessWidget {
+  const CammyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<Color> cards = [Colors.red, Colors.blue, Colors.green, Colors.orange];
-
-    return Scaffold(
-      appBar: AppBar(title: const Text("Cammy Cleanup")),
-      body: Center(
-        child: SizedBox(
-          height: 500,
-          child: CardSwiper(
-            cardsCount: cards.length,
-            cardBuilder: (context, index, _, __) => PhotoCard(color: cards[index], index: index),
-            onSwipe: (prev, current, direction) {
-              print("Swiped ${direction.name}");
-              return true;
-            },
-          ),
-        ),
+    return MaterialApp(
+      title: 'Cammy',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const MainLayout(),
     );
   }
 }
