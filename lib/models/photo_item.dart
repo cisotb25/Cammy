90e65c056cb706d72ca8e2a75a48bf9e11d31a55
Narciso Barroso <up202305431@up.pt>
2026-02-10 08:44:56 +0000

    Updated photo item to keep track of trash or keep

--- lib/models/photo_item.dart
+++ lib/models/photo_item.dart
@@ -1,8 +1,19 @@
 import 'package:photo_manager/photo_manager.dart';
 
+// Possible states for a photo
+enum PhotoStatus { unorganized, keep, delete }
+
 class PhotoItem {
   final String id;
-  final AssetEntity asset; // This is the real file reference
+  final AssetEntity asset;
+  PhotoStatus status; // Track if it's kept or deleted
 
-  PhotoItem({required this.id, required this.asset});
+  PhotoItem({
+    required this.id,
+    required this.asset,
+    this.status = PhotoStatus.unorganized
+  });
+
+  // Helper to get the date of the photo
+  DateTime get date => asset.createDateTime;
 }
